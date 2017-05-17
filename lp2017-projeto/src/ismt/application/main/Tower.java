087a1b5a2642a514453db933501aa2b205313d12
Andre <andremonteiro@ismt.pt>
2017-05-17 09:59:16 +0100

    17

--- lp2017-projeto/src/ismt/application/main/Tower.java
+++ lp2017-projeto/src/ismt/application/main/Tower.java
@@ -1,9 +1,52 @@
 package ismt.application.main;
 
 public class Tower{
-	String name;
-	int level;
-	int hitPoints;
-	int damagePoints;
-	int damagePerSecond;
+	private String name;
+	private int level;
+	private int hitPoints;
+	private int damagePoints;
+	private int damagePerSecond;
+
+	public Tower() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+	public int getDamagePoints() {
+		return damagePoints;
+	}
+
+	public void setDamagePoints(int damagePoints) {
+		this.damagePoints = damagePoints;
+	}
+
+	public int getDamagePerSecond() {
+		return damagePerSecond;
+	}
+
+	public void setDamagePerSecond(int damagePerSecond) {
+		this.damagePerSecond = damagePerSecond;
+	}
 }
